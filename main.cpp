909828cceca0291da159a49a8be2b9e97a4d30c7
Kent_Zhang <642238553@qq.com>
2022-08-20 21:52:24 +0800

    New Year Chaos issue

--- main.cpp
+++ main.cpp
@@ -3,85 +3,45 @@
 #include <string>
 #include <map>
 
-bool isClamp(int v, int minv, int maxv) {
-    return (v >= minv) && (v <= maxv);
+static inline int intMax(int a, int b) {
+    return a>b?a:b;
 }
 
-bool check(std::string &s, int posa, int posb) {
-    char a = s[posa];
-    char b = s[posb];
-
-    if (!isClamp(posa, 0, s.length() - 1)
-    || !isClamp(posb, 0, s.length() -1))
-        return false;
-    if (s[posa] == s[posb])
-        return true;
-    else
-        return false;
+static inline void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
 }
 
-bool checkPalidrome(std::string &s, int posa, int posb) {
-    while (posa < posb) {
-        if (check(s, posa, posb)) {
-            posa++;
-            posb--;
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+void minimumBribes(std::vector<int> q) {
+    int sumBribes = 0;
+    for (int i = q.size(); i>=1;) {
+        if ((q[i - 1] - i) > 2) {
+            printf("Too chaotic\n");
+            return;
         }
-        else {
-            return false;
-        }
-    }
-    return true;
-}
-
-int removeOneToPalindrome(std::string s) {
-    int posa = 0;
-    int posb = s.length() - 1;
-    int pos = -1;
-    while (posa < posb) {
-        if (check(s, posa, posb)) {
-            posa++;
-            posb--;
-        }else {
-            bool checkleft = check(s, posa + 1, posb);
-            bool checkright = check(s, posa, posb - 1);
-            if (checkleft) {
-                pos = posa;
-                if (checkPalidrome(s, posa + 1, posb))
-                    return pos;
-            }
-            if (checkright) {
-                pos = posb;
-                if (checkPalidrome(s, posa, posb - 1))
-                    return pos;
-            }
-            return -1;
+        int bribes = intMax(q[i - 1] - i, 0);
+        sumBribes += bribes;
+        if (bribes == 1) {
+            swap(q[i - 1], q[i]);
+        } else if (bribes == 2) {
+            swap(q[i - 1], q[i]);
+            swap(q[i], q[i + 1]);
+        } else {
+            i--;
         }
     }
-    return pos;
+    printf("%d\n", sumBribes);
 }
 
 int main() {
-    std::string s = "qerpiupiucva";
-    std::string target = s;
-    for (char x : s) {
-        target.insert(0, 1, x);
-    }
-    target.insert(16, 1, 'm');
-    std::cout << removeOneToPalindrome(target) << std::endl;
-    std::cout << removeOneToPalindrome("") << std::endl;
-    std::cout << removeOneToPalindrome("aa") << std::endl;
-    std::cout << removeOneToPalindrome("ab") << std::endl;
-    std::cout << removeOneToPalindrome("aba") << std::endl;
-    std::cout << removeOneToPalindrome("abab") << std::endl;
-    std::cout << removeOneToPalindrome("ababa") << std::endl;
-    std::cout << removeOneToPalindrome("abba") << std::endl;
-    std::cout << removeOneToPalindrome("hgygsvlfcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcwflvsgygh") << std::endl;
-    std::string x = "hgygsvlfcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcwflvsgygh";
-    std::cout << x << std::endl;
-    x.erase(44, 1);
-    std::cout << x << std::endl;
-    std::reverse(x.begin(), x.end());
-    std::cout << x << std::endl;
+//    minimumBribes({5,1,2,3,7,8,6,4});
+    minimumBribes({1,2,5,3,7,8,6,4});
 
     return 0;
 }
