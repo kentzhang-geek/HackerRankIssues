caa758bf407d6ca3e604e3c3ff685c93df5a29b7
Kent_Zhang <642238553@qq.com>
2022-08-24 20:54:22 +0800

    Jessies Cookie learned

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <queue>
 #include <map>
 using namespace std;
 
@@ -21,89 +22,50 @@ using namespace std;
  *  1. INTEGER n
  *  2. INTEGER m
  */
-#define MOD 1000000007
-std::vector<long long int> patternAcc;
-
-long long int sum(std::vector<long long int> & arr, long long int s, long long int n) {
-    long long int ret = 0;
-    for (long long int i = 0; i < n;i++) {
-        ret += arr[s + i];
-    }
-    return ret;
+int formula(int _1st, int _2nd) {
+    return _1st + 2 * _2nd;
 }
 
-long long int tetranacci(long long int n) {
-    std::vector<long long int> arr = {1,2,4,8};
-    if (n <= 4) {
-        long long int sum = 1;
-        for (long long int i = 0; i < n; i++) {
-            sum *= arr[i];
+int cookies(int k, vector<int> A) {
+    if (A.size() == 1)
+        return A[0] >= k ? 0 : -1;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pqueue;
+    for (auto v : A) {
+        pqueue.push(v);
+    }
+    int loop_count = 0;
+    bool ans = true;
+    while (1) {
+        if (pqueue.empty()) {
+            ans = false;
+            break;
         }
-        return sum;
+        int v1 = pqueue.top();
+        pqueue.pop();
+        if (v1 >= k) {
+            break;
+        }
+        if (pqueue.empty()) {
+            ans = false;
+            break;
+        }
+        int v2 = pqueue.top();
+        pqueue.pop();
+//        if (v2 >= k) {
+//            break;
+//        }
+        int res = formula(v1, v2);
+        pqueue.push(res);
+        loop_count++;
     }
-
-    for (long long int i = 4; i <= n; i++) {
-        arr.push_back(sum(arr, i - 4, 4));
-    }
-    long long int ret = 0;
-    return arr.back();
-}
-
-long long int patternsOf(long long int m) {
-    if (patternAcc.size() > m) {
-        return patternAcc[m];
-    }
-    long long int ret = 0;
-    if (m > 0)
-    {
-        ret = patternsOf(m - 1) + patternsOf(m - 2) + patternsOf(m - 3) + patternsOf(m - 4);
-        patternAcc.push_back(ret);
-    }
-    else if (m == 0) {
-        ret = 1;
-        patternAcc.push_back(1);
-    }
-    else
-        ret = 0;
-    return ret;
-}
-
-std::map<std::pair<long long int,long long int>, long long int> allblockMap;
-
-long long int allBlocks(long long int n, long long int m) {
-    if (allblockMap.find({n,m})!=allblockMap.end()) {
-        return allblockMap.at({n,m});
-    }
-    long long int ret = 1;
-    long long int p = patternsOf(m);
-    for (long long int i = 0; i < n; i++) {
-        ret *= p;
-    }
-    allblockMap[{n,m}] = ret;
-    return ret;
-}
-
-std::map<std::pair<long long int,long long int>, long long int> blockMap;
-long long int legoBlocks(long long int n, long long int m) {
-    if (blockMap.find({n,m})!=blockMap.end()) {
-        return blockMap.at({n,m});
-    }
-    long long int result = 0;
-    result = allBlocks(n, m);
-    for (long long int i = 1; i <= m - 1; i++) {
-        result -= legoBlocks(n, i) * allBlocks(n, m - i);
-    }
-    blockMap[{n,m}] = result;
-    return result;
+    if (ans)
+        return loop_count;
+    return -1;
 }
 
 int main() {
-    printf("%d\n", legoBlocks(8,6) % MOD);
-    printf("%d\n", legoBlocks(10,8));
-    printf("%d\n", legoBlocks(7,3));
-    printf("%d\n", legoBlocks(529, 190));
-    printf("%d\n", legoBlocks(4,4));
-    printf("%d\n", legoBlocks(4,5));
-    printf("%d\n", legoBlocks(4,6));
+    printf("%d\n", cookies(7,{1,2,3,9,10,12}));
+    printf("%d\n", cookies(90,{13,47,74,12,89,74,18,38}));
+    printf("%d\n", cookies(10,{1,1,1}));
     return 0;
 }
