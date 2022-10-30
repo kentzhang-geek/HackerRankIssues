92941ab6837742ccc3af289f53a2b1acee9a1b25
kent <642238553@qq.com>
2022-10-30 23:42:39 +0800

    XOR string problem solved

--- main.cpp
+++ main.cpp
@@ -1,85 +1,29 @@
-#include <bits/stdc++.h>
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
+string strings_xor(string s, string t) {
 
-/*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-void miniMaxSum(vector<int> arr) {
-    long long maxNum = arr[0];
-    long long minNum = arr[0];
-    long long sum = 0;
-    for (int &num:arr) {
-        maxNum = max(maxNum, (long long)num);
-        minNum = min(minNum, (long long)num);
-        sum += num;
+    string res = "";
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == t[i])
+            res += '0';
+        else
+            res += '1';
     }
-    printf("%ld %ld\n", sum - maxNum, sum - minNum);
+
+    return res;
 }
 
-int main()
-{
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-    vector<int> arr(5);
-
-    for (int i = 0; i < 5; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
-    }
-
-    miniMaxSum(arr);
-
+int main() {
+    string s, t;
+    cin >> s >> t;
+    cout << strings_xor(s, t) << endl;
     return 0;
 }
 
-string ltrim(const string &str) {
-    string s(str);
 
-    s.erase(
-            s.begin(),
-            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
 
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-            find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-            s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
-}
